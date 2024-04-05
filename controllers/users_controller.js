2f06c73ae7c0830dde1e7ccfc2712d1084ef9ef2
Dean12 <mahyudinakbar10@gmail.com>
2024-04-05 17:37:16 +0700

    login done;

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,42 @@
 const pool = require ('../config/config.js')
-const {hashPassword} = require('../helpers/utils.js')
-
+const {hashPassword, bcrypt} = require('../helpers/utils.js')
+const jwt = require('jsonwebtoken');
+const {secretKey,tokenValid} = require('../helpers/utils.js')
 class Controller {
+    static async postLogin(req, res, next) {
+        try {
+            const { email, password } = req.body;
+            const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+            if (!regex.test(email)) {
+                return res.status(400).json({
+                    status: 102,
+                    message: "Parameter email tidak sesuai format",
+                    data: null
+                });
+            }
+            const query = await pool.query(`SELECT * FROM users WHERE email = $1`, [email]);
+            const user = query.rows[0];
+            if (!user || !(await bcrypt.compare(password, user.password))) {
+                return res.status(404).json({
+                    status: 103,
+                    message: "Email atau password salah",
+                    data: null
+                });
+            } 
+            const token = jwt.sign({user:user.email},secretKey);
+            return res.status(200).json({
+                status: 0,
+                message: "Login Sukses",
+                data: {
+                    token:token
+                }
+            })
+        } catch (error) {
+            console.log(error);
+            next(error);
+        }
+    }    
+
     static async getUsers(req,res,next){
         try {
             const data = await new Promise((resolve, reject) => {
--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const Controller = require('../controllers/users_controller')
+const Controller = require('../controllers/users_controller');
 
-router.get('/', Controller.getUsers);
-
+router.post('/', Controller.postLogin);
 module.exports = router;
